ad4803f770d81debd37100e67d56369547ce07e6
shsdev <shsschlarb-dev@yahoo.de>
2015-01-14 16:56:30 +0100

    Clean App class

--- src/main/java/ait/ac/at/eark/App.java
+++ src/main/java/ait/ac/at/eark/App.java
@@ -75,9 +75,7 @@ public class App {
 	protected Logger logger = LoggerFactory
 			.getLogger(this.getClass().getName());
 
-	public static void main(String[] args) throws IOException,
-			FileNotFoundException, UserException, RepositoryException,
-			SubsystemNotAvailableException {
+	public static void main(String[] args) throws IOException, FileNotFoundException, UserException, RepositoryException, SubsystemNotAvailableException {
 		File sipExampleFile = new File(SIP_EXAMPLE_PATH);
 		File sipIngestDir = new File(INGEST_AREA_SUBDIR);
 		FileUtils.copyFileToDirectory(sipExampleFile, sipIngestDir);
@@ -85,262 +83,29 @@ public class App {
 		app.start();
 	}
 
-	private void start() throws IOException, FileNotFoundException,
-			UserException, RepositoryException, SubsystemNotAvailableException {
-
-		cleanDb();		
-
+	private void start() throws IOException, FileNotFoundException,	UserException, RepositoryException, SubsystemNotAvailableException {
+		cleanDb();
 		try {
-			
 			try {
-				HibernateUtil
-						.init("/home/shs/workspace/sip2aiptest/conf/hibernateCentralDB.cfg.xml");
+				HibernateUtil.init("/home/shs/workspace/sip2aiptest/conf/hibernateCentralDB.cfg.xml");
 			} catch (Exception e) {
 				System.out.println("Hibernate initialization error");
 			}
-			
-			AbstractApplicationContext ctx =
-					new FileSystemXmlApplicationContext("conf/spring-config.xml");
+
+			AbstractApplicationContext ctx = new FileSystemXmlApplicationContext(
+					"conf/spring-config.xml");
 			ctx.registerShutdownHook();
-			
+
 			ActionFactory af = (ActionFactory) ctx.getBean("actionFactory");
 			AbstractAction action = af.buildNextAction();
-			while(action != null) {
+			while (action != null) {
 				action.run();
 				action = af.buildNextAction();
 			}
-			
-			
-
-			// ActionFactory af = (ActionFactory) ctx.getBean("actionFactory");
-
-			// ActionFactory af = (ActionFactory) ctx.getBean("actionFactory");
-			// af.buildNextAction();
-			// AbstractAction action = af.buildNextAction();
-
-			// ActionRegistry ar = (ActionRegistry)
-			// ctx.getBean("actionRegistry");
-
-			// ////////////////////
-			// unpack action
-			// ////////////////////
-			// UnpackAction ua = (UnpackAction)
-			// ctx.getBean("IngestUnpackAction");
-			// ua.setJob(job);
-			// // ua.setIngestGate(ig);
-			// ua.setPSystem(ps);
-			// ua.setObject(object);
-			// ua.setLocalNode(localNode);
-			// object.setTransientNodeRef(localNode);
-			// ar.registerAction(ua);
-			// //ua.setActionMap(ar);
-			// ua.run();
-
-			// ////////////////////
-			// restructure action
-			// ////////////////////
-			// RestructureAction ra = new RestructureAction();
-			// FileFormatFacade fileFormatFacade = new
-			// StandardFileFormatFacade();
-			// GridFacade gf = new FakeGridFacade();
-			// ra.setName("restructuring");
-			// ua.setStartStatus("110");
-			// ua.setEndStatus("120");
-			// ra.setFileFormatFacade(fileFormatFacade);
-			// ra.setGridRoot(gf);
-			// ra.setIngestGate(ig);
-			// ra.setObject(object);
-			// ra.setLocalNode(localNode);
-			// ra.setJob(job);
-			// object.setTransientNodeRef(localNode);
-			// ra.setPSystem(ps);
-			// ar.registerAction(ra);
-			// ra.setActionMap(ar);
-			// ra.run();
-			//
-			// // ////////////////////
-			// // validate metadata action
-			// // ////////////////////
-			// ValidateMetadataAction vma = new ValidateMetadataAction();
-			// MetadataStructureFactory msf = new MetadataStructureFactory();
-			// vma.setName("validation");
-			// vma.setStartStatus("120");
-			// vma.setEndStatus("130");
-			// vma.setObject(object);
-			// vma.setLocalNode(localNode);
-			// vma.setMsf(msf);
-			// vma.setJob(job);
-			// vma.setPSystem(ps);
-			// ar.registerAction(vma);
-			// vma.setActionMap(ar);
-			// vma.run();
-			//
-			// // ////////////////////
-			// // scan action
-			// // ////////////////////
-			// ScanAction sca = new ScanAction();
-			// DistributedConversionAdapter dca = new
-			// FakeDistributedConversionAdapter();
-			// sca.setDistributedConversionAdapter(dca);
-			// sca.setName("scanning");
-			// sca.setStartStatus("130");
-			// sca.setEndStatus("140");
-			// sca.setObject(object);
-			// sca.setLocalNode(localNode);
-			// sca.setJob(job);
-			// sca.setPSystem(ps);
-			// ar.registerAction(sca);
-			// sca.setActionMap(ar);
-			// sca.run();
-			//
-			// // ////////////////////
-			// // register urn action
-			// // ////////////////////
-			// RegisterURNAction rua = new RegisterURNAction();
-			// rua.setName("scanning");
-			// rua.setStartStatus("140");
-			// rua.setEndStatus("150");
-			// rua.setObject(object);
-			// rua.setLocalNode(localNode);
-			// rua.setJob(job);
-			// rua.setPSystem(ps);
-			// ar.registerAction(rua);
-			// rua.setActionMap(ar);
-			// rua.run();
-			//
-			// // ////////////////////
-			// // convert action
-			// // ////////////////////
-			// ConvertAction conva = new ConvertAction();
-			// conva.setDistributedConversionAdapter(dca);
-			// conva.setName("converting");
-			// conva.setStartStatus("150");
-			// conva.setEndStatus("160");
-			// conva.setObject(object);
-			// conva.setLocalNode(localNode);
-			// conva.setJob(job);
-			// conva.setPSystem(ps);
-			// ar.registerAction(conva);
-			// conva.setActionMap(ar);
-			// conva.run();
-			//
-			// // ////////////////////
-			// // update metadata action
-			// // ////////////////////
-			// UpdateMetadataAction uma = new UpdateMetadataAction();
-			// uma.setName("updatingmetadata");
-			// uma.setStartStatus("160");
-			// uma.setEndStatus("170");
-			// uma.setObject(object);
-			// uma.setLocalNode(localNode);
-			// uma.setJob(job);
-			// uma.setPSystem(ps);
-			// ar.registerAction(uma);
-			// uma.setActionMap(ar);
-			// uma.run();
-			//
-			// // ////////////////////
-			// // check formats action
-			// // ////////////////////
-			// CheckFormatsAction chkfmtsa = new CheckFormatsAction();
-			// chkfmtsa.setFileFormatFacade(fileFormatFacade);
-			// chkfmtsa.setName("checkingformats");
-			// chkfmtsa.setStartStatus("170");
-			// chkfmtsa.setEndStatus("180");
-			// chkfmtsa.setObject(object);
-			// chkfmtsa.setLocalNode(localNode);
-			// chkfmtsa.setJob(job);
-			// chkfmtsa.setPSystem(ps);
-			// ar.registerAction(chkfmtsa);
-			// chkfmtsa.setActionMap(ar);
-			// chkfmtsa.run();
-			//
-			// // ////////////////////
-			// // create premis action
-			// // ////////////////////
-			// CreatePremisAction cpa = new CreatePremisAction();
-			// cpa.setFileFormatFacade(fileFormatFacade);
-			// cpa.setName("creatingpremis");
-			// cpa.setStartStatus("188");
-			// cpa.setEndStatus("190");
-			// cpa.setObject(object);
-			// cpa.setLocalNode(localNode);
-			// cpa.setJob(job);
-			// cpa.setPSystem(ps);
-			// ar.registerAction(cpa);
-			// cpa.setActionMap(ar);
-			// cpa.run();
-			//
-			// // ////////////////////
-			// // scan for presentation action
-			// // ////////////////////
-			// ScanForPresentationAction scfpa = new
-			// ScanForPresentationAction();
-			// scfpa.setFormatScanService(fileFormatFacade);
-			// scfpa.setDistributedConversionAdapter(dca);
-			// scfpa.setName("scanningforpresentation");
-			// scfpa.setStartStatus("180");
-			// scfpa.setEndStatus("186");
-			// scfpa.setObject(object);
-			// scfpa.setLocalNode(localNode);
-			// scfpa.setJob(job);
-			// scfpa.setPSystem(ps);
-			// ar.registerAction(scfpa);
-			// scfpa.setActionMap(ar);
-			// scfpa.run();
-			//
-			// // ////////////////////
-			// // convert for presentation action
-			// // ////////////////////
-			// ConvertAction convfpa = new ConvertAction();
-			// convfpa.setDistributedConversionAdapter(dca);
-			// convfpa.setName("convertingforpresentation");
-			// convfpa.setStartStatus("186");
-			// convfpa.setEndStatus("188");
-			// convfpa.setObject(object);
-			// convfpa.setLocalNode(localNode);
-			// convfpa.setJob(job);
-			// convfpa.setPSystem(ps);
-			// ar.registerAction(convfpa);
-			// convfpa.setActionMap(ar);
-			// convfpa.run();
-			//
-			// // ////////////////////
-			// // build aip action
-			// // ////////////////////
-			// BuildAIPAction baipa = new BuildAIPAction();
-			// baipa.setName("buildingaip");
-			// baipa.setStartStatus("190");
-			// baipa.setEndStatus("200");
-			// baipa.setObject(object);
-			// baipa.setLocalNode(localNode);
-			// baipa.setJob(job);
-			// baipa.setPSystem(ps);
-			// ar.registerAction(baipa);
-			// baipa.setActionMap(ar);
-			// baipa.run();
-			//
-			// // ////////////////////
-			// // tar action
-			// // ////////////////////
-			// TarAction tara = new TarAction();
-			// tara.setDistributedConversionAdapter(dca);
-			// tara.setName("tarpackaging");
-			// tara.setStartStatus("200");
-			// tara.setEndStatus("210");
-			// tara.setObject(object);
-			// tara.setLocalNode(localNode);
-			// tara.setJob(job);
-			// tara.setPSystem(ps);
-			// ar.registerAction(tara);
-			// tara.setActionMap(ar);
-			// tara.run();
-
 		} finally {
-//			FileUtils.deleteDirectory(new File(
-//					"/home/shs/workspace/sip2aiptest/storage/WorkArea/work"));
+			// FileUtils.deleteDirectory(new File(
+			// "/home/shs/workspace/sip2aiptest/storage/WorkArea/work"));
 		}
-
 	}
 
 	private void cleanDb() {
@@ -355,10 +120,7 @@ public class App {
 		hsqlServer.start();
 		try {
 			Class.forName("org.hsqldb.jdbcDriver");
-			connection = DriverManager
-					.getConnection(
-							"jdbc:hsqldb:file:/home/shs/workspace/DNSCore/ContentBroker/mydb",
-							"sa", "");
+			connection = DriverManager.getConnection("jdbc:hsqldb:file:/home/shs/workspace/DNSCore/ContentBroker/mydb",	"sa", "");
 			connection.prepareStatement("DELETE FROM DOCUMENTS;").execute();
 			connection.prepareStatement("DELETE FROM EVENTS;").execute();
 			connection.prepareStatement("DELETE FROM CONVERSION_QUEUE;")
